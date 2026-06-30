92fc0a69cbe618755621b6489303abcd0ab9e160
mann-jain143 <mannjain4885@gmail.com>
2026-06-30 11:58:50 +0530

    Setup GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/My-portfolio/', // This is the crucial part that fixes 404s on GitHub Pages
+  base: '/My-portfolio/',
 })
